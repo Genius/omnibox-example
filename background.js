5d5bf65abe2f420642c1a452348bea042950f2d3
John Crepezzi <john@genius.com>
2015-06-04 12:34:13 -0400

    Remove outdated access token

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.omnibox.onInputChanged.addListener(function (query, suggest) {
-  var accessToken = '4Haevzgr5JIoLkHl1TdfRDJ4jy2PsPKQEfQzTR5_BIgJHty-uAaoWo3WfGCkCbnn';
+  var accessToken = '<your-access-token>';
 
   fetch('https://api.genius.com/search?access_token=' + accessToken + '&q=' + encodeURIComponent(query)).then(function (response) {
     response.json().then(function (data) {
